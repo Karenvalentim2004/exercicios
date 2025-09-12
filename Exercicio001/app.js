53a32974135dbf9ec0ab379bca64574dab5b9d6a
Karen Valentim <karenvalentim54@gmail.com>
2025-09-12 20:18:46 -0300

    Criação dos exercício 01 e 02

---
+++ Exercicio001/app.js
@@ -0,0 +1,8 @@
+let nome = "", idade = 0
+nome = prompt("Informe o seu nome:")
+idade = prompt("Informe a sua idade:")
+alert("Olá, seja bem vindo(a) " + nome + "\n Você tem " + idade + " anos")
+
+console.log("Olá, seja bem vindo(a) " + nome + "\n Você tem " + idade + " anos")
+
+document.write("Olá, seja bem vindo(a) " + nome + "<br> Você tem " + idade + " anos")
